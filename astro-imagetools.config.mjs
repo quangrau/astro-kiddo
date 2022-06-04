d723f65053ac1cc7730f62d03042e653b1909e79
Quang Rau <lethanhquang910@gmail.com>
2022-06-05 00:23:13 +0800

    feat: adding SSR

--- astro-imagetools.config.mjs
+++ astro-imagetools.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro-imagetools/config";
 
 export default defineConfig({
   includeSourceFormat: false,
-  sizes: "(min-width: 350px) 1000px, 100vw",
+  sizes: "(min-width: 300px) 1000px, 100vw",
   decoding: "auto",
 });
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-// import { astroImageTools } from "astro-imagetools";
+import { astroImageTools } from "astro-imagetools";
 import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  outDir: './dist',
   // adapter: netlify(),
-  // integrations: [
-  //   astroImageTools,
-  // ]
+  // site: 'https://astro-kiddo.netlify.app'
+  integrations: [
+    astroImageTools,
+  ]
 });
