6a2bbbd87aa54c0c7308e8abf7e3a1946edd2e97
Quang Rau <lethanhquang910@gmail.com>
2022-06-05 01:24:44 +0800

    feat: update colors constrast

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  // adapter: netlify(),
-  // site: 'https://astro-kiddo.netlify.app'
+  adapter: netlify(),
+  site: 'https://astro-kiddo.netlify.app',
   integrations: [
     astroImageTools,
   ]
